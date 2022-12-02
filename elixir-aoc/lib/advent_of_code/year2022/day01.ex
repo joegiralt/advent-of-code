c4b4cdcd2a84c2b4c30e69368f680114a4e379be
Joseph Martin Giralt <joe.m.giralt@gmail.com>
2022-12-01 22:45:17 -0500

    remove benchee from solution

--- elixir-aoc/lib/advent_of_code/year2022/day01.ex
+++ elixir-aoc/lib/advent_of_code/year2022/day01.ex
@@ -34,8 +34,3 @@ defmodule AdventOfCode.Year2022.Day01 do
     |> Enum.sum()
   end
 end
-
-Benchee.run(%{
-  "Day 1, Part 1" => fn -> AdventOfCode.Year2022.Day01 end,
-  "Day 1, Part 2" => fn -> AdventOfCode.Year2022.Day02 end
-})
